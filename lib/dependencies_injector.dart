94ff34ad73d215db049c116c5fca430dd980ae7d
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-09-28 00:49:35 +0700

    add local notification init

--- lib/dependencies_injector.dart
+++ lib/dependencies_injector.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get_it/get_it.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
@@ -19,7 +20,31 @@ Future<void> init() async {
     await SettingsRepository.hoyoverseDeviceId.set(const Uuid().v4());
   }
 
+  // await initLocalNotification();
+
   final getit = GetIt.instance;
 
   getit.registerSingleton<Isar>(isar);
+}
+
+Future<void> initLocalNotification() async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  final DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings(
+    onDidReceiveLocalNotification: (int id, String? title, String? body, String? payload) {},
+  );
+  const LinuxInitializationSettings initializationSettingsLinux =
+      LinuxInitializationSettings(defaultActionName: 'Open notification');
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsDarwin,
+      macOS: initializationSettingsDarwin,
+      linux: initializationSettingsLinux);
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (response) {},
+    onDidReceiveBackgroundNotificationResponse: (response) {},
+  );
 }
